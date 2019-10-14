375cb3c372b613ba63cb2be8ce1f7a94b15778d5
Phil Borlin <phil@borlin.net>
2019-10-14 15:14:53 -0600

    Add debug statements

--- e2e/wal.go
+++ e2e/wal.go
@@ -7,23 +7,24 @@ import (
 	"strings"
 
 	"github.com/philborlin/committed/topic"
+	"github.com/pkg/errors"
 )
 
 func findStringInTopic(topicName string, node int, s string) error {
 	t, err := topic.Restore(topicName, fmt.Sprintf("./Data/raft-%d/%s", node, topicName))
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "Error restoring %s", fmt.Sprintf("./Data/raft-%d/%s", node, topicName))
 	}
 
 	r, err := t.NewReader(0)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "Error creating NewReader")
 	}
 
 	for {
 		p, err := r.Next(context.Background())
 		if err != nil {
-			return err
+			return errors.Wrap(err, "Error getting next")
 		}
 
 		b := p.Data
@@ -33,37 +34,3 @@ func findStringInTopic(topicName string, node int, s string) error {
 		}
 	}
 }
-
-// func getWalReader(topic string, node int) (types.LiveReader, error) {
-// 	factory := &types.TSDBWALFactory{}
-
-// 	sr := wal.SegmentRange{Dir: fmt.Sprintf("./Data/raft-%d/%s", node, topic), First: 0, Last: -1}
-// 	srr, err := factory.NewSegmentsRangeReader(sr)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	lr := factory.NewLiveReader(nil, nil, srr)
-
-// 	return lr, nil
-// }
-
-// func findStringInReader(r types.LiveReader, s string, id int) error {
-// 	for r.Next() {
-// 		b := r.Record()
-// 		log.Printf("[%d] Looking for [%s] in [%s]", id, s, string(b))
-// 		if strings.Contains(string(b), s) {
-// 			return nil
-// 		}
-// 	}
-
-// 	return fmt.Errorf("string [%s] did not exist in wal", s)
-// }
-
-// func findStringInTopicReader(topic string, s string, node int) error {
-// 	r, err := getWalReader(topic, node)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return findStringInReader(r, s, node)
-// }
