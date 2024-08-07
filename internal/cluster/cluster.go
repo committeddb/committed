5c7db3d934a38fce3474abe142dfa599402f2e20
Phil Borlin <phil@risertech.com>
2024-08-07 15:44:31 -0600

    Add cluster interface (which db implements)

--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -1,4 +1,16 @@
 package cluster
 
+import "context"
+
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 //go:generate protoc --go_out=paths=source_relative:. ./cluster.proto
+
+type Cluster interface {
+	Propose(p *Proposal)
+	ProposeType(t *Type) error
+	ProposeDeleteType(id string) error
+	Type(id string) (*Type, error)
+	Close() error
+	// The caller should run this on a separate go routine - or do we want to do this so close() can cancel all contexts?
+	Sync(ctx context.Context, id string, s Syncable) error
+}
